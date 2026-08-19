dd5c3fb86f49f6d2dc6372782091236ba43d4ddd
Szuu1 <suanchristopherb220100815@gmail.com>
2026-08-19 08:33:50 +0800

    Feat: add React Router navigation for portfolio pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css'
+import Home from './pages/Home.jsx'
+import About from './pages/About.jsx'
+import Contact from './pages/Contact.jsx'
+import Interests from './pages/Interests.jsx'
 import App from './App.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/Home",
+    element: <Home />,
+    errorElement: <Error />
+  },
+  {
+    path: "/About",
+    element: <About />,
+    errorElement: <Error /> 
+  },
+  {
+    path: "/Contact",
+    element: <Contact />,
+    errorElement: <Error /> 
+  },
+  {
+    path: "/Interests",
+    element: <Interests />,
+    errorElement: <Error />
+  } 
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
